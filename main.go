fe4a8c24136a9fd6c87d69a0d03182deb92d27ea
annietse <annietse03@gmail.com>
2022-11-02 13:46:51 +0000

    select menu

--- main.go
+++ main.go
@@ -3,20 +3,79 @@ package main
 import (
 	"fmt"
 	tea "github.com/charmbracelet/bubbletea"
+	"os"
 )
 
 func NewModel() (*model, error) {
-	return model{}, nil
+	return &model{}, nil
 }
 
 type model struct {
-	nameInput string
-	listInput string
-	event     string
+	choices  []string
+	cursor   int
+	selected map[int]struct{}
 }
 
-func main() {
-	fmt.Println("Hello world")
+func initialModel() model {
+	return model{
+		choices:  []string{"add", "push", "fetch", "commit", "pull"},
+		selected: make(map[int]struct{}),
+	}
+}
 
-	var _ tea.Model = (*model)(nil)
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	s := "What option would you like? \n\n"
+	for i, choice := range m.choices {
+		cursor := " " //no cursor
+		if m.cursor == i {
+			cursor = ">"
+		}
+		//is this choice selected?
+		checked := " "
+		if _, ok := m.selected[i]; ok {
+			checked = "x"
+		}
+
+		//render the row
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+	s += "\nPress q to quit\n"
+	return s
+}
+func main() {
+	p := tea.NewProgram(initialModel())
+	if err := p.Start(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
 }
